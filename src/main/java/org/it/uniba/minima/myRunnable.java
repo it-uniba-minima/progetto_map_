d371af766812f09d61590c8a20b7a6035206aeae
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-17 23:59:04 +0200

    Modificata la classe myRunnable affinché diventasse Singleton

--- src/main/java/org/it/uniba/minima/myRunnable.java
+++ src/main/java/org/it/uniba/minima/myRunnable.java
@@ -6,13 +6,23 @@ import javax.sound.sampled.*;
 public class myRunnable extends Thread {
     private static Clip clip;
     private static boolean running = false;
+    private static myRunnable instance;
+
+    public static myRunnable getInstance()  {
+        if (instance == null) {
+            instance = new myRunnable();
+        }
+        return instance;
+    }
+
     @Override
     public void run() {
+        // TODO: Change the placeholder audio file with the actual audio file
         running = true;
         try {
-            File file = new File("docs/audio/Avventura-nella-piramide-egizia.wav");
+            File file = new File("docs/audio/Marco.wav");
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-            Clip clip = AudioSystem.getClip();
+            clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start();
             clip.loop(Clip.LOOP_CONTINUOUSLY);
@@ -20,12 +30,17 @@ public class myRunnable extends Thread {
             System.out.println(e);
         }
     }
+    public static void startClip() {
+        running = true;
+        clip.start();
+    }
+
     public static void stopClip() {
         running = false;
         clip.stop();
     }
 
-    public boolean isRunning() {
+    public static boolean isRunning() {
         return running;
     }
 }
