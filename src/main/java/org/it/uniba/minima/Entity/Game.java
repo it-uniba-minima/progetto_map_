152c4ec1036f1e9c92337d6cc8949af0683e06c5
Pascoooo <pacucciniko@gmail.com>
2024-05-23 13:12:42 +0200

    Aggiunta metodi set, get e getInstance alla classe Game

--- src/main/java/org/it/uniba/minima/Entity/Game.java
+++ src/main/java/org/it/uniba/minima/Entity/Game.java
@@ -1,13 +1,27 @@
 package org.it.uniba.minima.Entity;
 
 
+import java.io.Serializable;
 import java.util.Map;
 
-public class Game {
+public class Game implements Serializable {
     private String nickname;
     private Object[] inventory;
     private String currentTime;
     private Room currentRoom;
     private Map<Integer, Boolean> graphMap;
     private Map<Integer, String> stateMap;
+
+    public static Game getInstance(Game game) {
+        return game;
+    }
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
