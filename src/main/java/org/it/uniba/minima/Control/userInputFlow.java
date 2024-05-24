f5798d9605755e51dae30284c09a8192249ccca2
Pascoooo <pacucciniko@gmail.com>
2024-05-24 17:56:03 +0200

    Creazione classe userInputFlow per la divisione dell'input tra Gameflow e Wordleflow

---
+++ src/main/java/org/it/uniba/minima/Control/userInputFlow.java
@@ -0,0 +1,17 @@
+package org.it.uniba.minima.Control;
+
+import org.it.uniba.minima.Boundary.WordleGame;
+import org.it.uniba.minima.GUI.GameGUI;
+import org.it.uniba.minima.GUI.Wordle;
+
+public class userInputFlow {
+
+    public static void Wordleflow(String text) {
+        WordleGame.printSplittedText(text.trim().toUpperCase());
+    }
+
+
+
+    public static void Gameflow(String text) {
+     }
+}
