54ff2f31fc5863794b1b5ab6903ef9fb0c7db812
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-28 17:54:51 +0200

    Aggiunto attributo isTalkable e relativo metodo per verificarlo

--- src/main/java/org/it/uniba/minima/Entity/Personage.java
+++ src/main/java/org/it/uniba/minima/Entity/Personage.java
@@ -1,6 +1,11 @@
 package org.it.uniba.minima.Entity;
 
 public class Personage extends Agent{
+    private boolean isTalkable;
 
     public Personage() {}
+
+    public boolean isTalkable() {
+        return isTalkable;
+    }
 }
