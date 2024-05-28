9ec17fcdd7b7a972c466dff9718465e68d42b259
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-28 12:00:58 +0200

    Modifica del parser per renderlo un estensione della classe CommandExecutorKey

--- src/main/java/org/it/uniba/minima/Type/ParserOutput.java
+++ src/main/java/org/it/uniba/minima/Type/ParserOutput.java
@@ -1,38 +1,11 @@
 package org.it.uniba.minima.Type;
-import org.it.uniba.minima.Entity.Agent;
+import org.it.uniba.minima.Control.CommandExecutorKey;
 
-public class ParserOutput {
-    private CommandType command;
-    private Agent agent1;
-    private Agent agent2;
+public class ParserOutput extends CommandExecutorKey {
     private int args;
 
     public ParserOutput() {
         args = 0;
-    }
-
-    public CommandType getCommand() {
-        return command;
-    }
-
-    public void setCommand(CommandType command) {
-        this.command = command;
-    }
-
-    public Agent getAgent1() {
-        return agent1;
-    }
-
-    public void setAgent1(Agent agent) {
-        this.agent1 = agent;
-    }
-
-    public Agent getAgent2() {
-        return agent2;
-    }
-
-    public void setAgent2(Agent agent2) {
-        this.agent2 = agent2;
     }
 
     public int getArgs() {
