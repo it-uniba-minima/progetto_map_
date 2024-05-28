ab44ca9257db8b92ac77d91dc1b14ea0e295c68c
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-28 11:57:13 +0200

    Refactor totale della classe per instradare la gestione dell'input in base all'evento corrente

--- src/main/java/org/it/uniba/minima/Control/userInputFlow.java
+++ src/main/java/org/it/uniba/minima/Control/userInputFlow.java
@@ -1,17 +1,48 @@
 package org.it.uniba.minima.Control;
 
 import org.it.uniba.minima.Boundary.WordleGame;
-import org.it.uniba.minima.GUI.GameGUI;
-import org.it.uniba.minima.GUI.Wordle;
+import org.it.uniba.minima.Boundary.outputDisplayManager;
+import org.it.uniba.minima.Type.ParserOutput;
 
 public class userInputFlow {
+    public static int Event = 0;
+    static Parser parser = new Parser();
+    private static CommandExecutor commandExecutor; // = new CommandExecutor();
 
-    public static void Wordleflow(String text) {
-        WordleGame.printSplittedText(text.trim().toUpperCase());
+    //WordleGame wordleGame = new WordleGame();
+
+    public static void GameFlow(String text) {
+        outputDisplayManager.displayText(text);
+        switch(Event) {
+            case 0:
+                parserFlow(text);
+                break;
+            case 1:
+                wordleFlow(text);
+                break;
+            case 2:
+                //call ParolaCriptata
+                break;
+            case 3:
+                //call Mattonelle
+                break;
+            default:
+                parserFlow(text);
+                break;
+        }
     }
 
+    private static void parserFlow(String text) {
+        ParserOutput output = parser.parse(text);
+        if (output.getArgs() != 0) {
+            commandExecutor.execute(output);
+        } else {
+            outputDisplayManager.displayText("Comando non valido");
+        }
 
+    }
 
-    public static void Gameflow(String text) {
-     }
+    public static void wordleFlow(String text) {
+        WordleGame.printSplittedText(text.trim().toUpperCase());
+    }
 }
