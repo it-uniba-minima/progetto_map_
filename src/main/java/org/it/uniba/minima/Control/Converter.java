e7a5e6c6bb42dc2e55d728c1567bccb192b58e31
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-28 18:04:00 +0200

    Modifiche per l'adattamento al flow del gioco

--- src/main/java/org/it/uniba/minima/Control/Converter.java
+++ src/main/java/org/it/uniba/minima/Control/Converter.java
@@ -2,12 +2,12 @@
 package org.it.uniba.minima.Control;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import org.it.uniba.minima.Entity.Agent;
 import org.it.uniba.minima.Entity.Game;
 import org.it.uniba.minima.Entity.Room;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -23,8 +23,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Converter {
+    List<Room> allRooms = new ArrayList<>();
+
     public Map<String, Agent> convertJsonToJavaClass() {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Agent.class, new AgentDeserializer())
+                .create();
         Map<String, Agent> allAgents = new HashMap<>();
 
         try {
@@ -32,14 +36,10 @@ public class Converter {
             File file = new File(url.toURI());
             JsonReader reader = new JsonReader(new FileReader(file));
             Type roomListType = new TypeToken<ArrayList<Room>>(){}.getType();
-            List<Room> roomList = gson.fromJson(reader, roomListType);
+            List<Room> roomList = gson.fromJson(reader, roomListType); // This gson is the one with the custom deserializer
             for (Room room : roomList) {
-                System.out.println(room.getName());
-                room.getAgents().forEach(agent ->
-                {
-                    System.out.println("Agent Name: " + agent.getName());
-                    allAgents.put(agent.getName(), agent);
-                });
+                allRooms.add(room);
+                room.getAgents().forEach(agent -> allAgents.put(agent.getName(), agent));
             }
         } catch (FileNotFoundException | URISyntaxException e) {
             e.printStackTrace();
@@ -52,7 +52,6 @@ public class Converter {
             Type agentListType = new TypeToken<ArrayList<Agent>>(){}.getType();
             List<Agent> agentList = gson.fromJson(reader, agentListType);
             agentList.forEach(agent -> allAgents.put(agent.getName(), agent));
-            System.out.println(agentList.getFirst().getName());
         } catch (FileNotFoundException | URISyntaxException e) {
             e.printStackTrace();
         }
@@ -64,8 +63,9 @@ public class Converter {
             URL url = getClass().getResource("/static/Game.json");
             File file = new File(url.toURI());
             JsonReader reader = new JsonReader(new FileReader(file));
-            Game game = gson.fromJson(reader, Game.class);
+            Game.setUpGame(gson.fromJson(reader, Game.class));
         } catch (FileNotFoundException | URISyntaxException e) {
+            e.printStackTrace();
         }
         return allAgents;
     }
@@ -93,11 +93,71 @@ public class Converter {
             e.printStackTrace();
         } */
 
-    public void ConvertGametoJson(Game game) throws IOException, ClassNotFoundException{
+    public void ConvertGametoJson() throws IOException, ClassNotFoundException{
         Gson gson = new Gson();
+        Game game = Game.getInstance();
+        /*
+        game.setNickname("Player");
+
+        game.setInventory(new ArrayList<>());
+
+        game.setCurrentTime("00:00:00");
+
+        game.setCurrentRoom(allRooms.get(0));
+
+        List<Corridor> corridorsMap = new ArrayList<>();
+        corridorsMap.add(new Corridor(allRooms.get(0), CommandType.NORD, true, allRooms.get(1)));
+        corridorsMap.add(new Corridor(allRooms.get(1), CommandType.SUD, false, allRooms.get(0)));
+
+        corridorsMap.add(new Corridor(allRooms.get(1), CommandType.EST, false, allRooms.get(3)));
+        corridorsMap.add(new Corridor(allRooms.get(3), CommandType.OVEST, false, allRooms.get(1)));
+
+        corridorsMap.add(new Corridor(allRooms.get(1), CommandType.OVEST, false, allRooms.get(2)));
+        corridorsMap.add(new Corridor(allRooms.get(2), CommandType.EST, false, allRooms.get(1)));
+
+        corridorsMap.add(new Corridor(allRooms.get(1), CommandType.NORD, true, allRooms.get(4)));
+        corridorsMap.add(new Corridor(allRooms.get(4), CommandType.SUD, false, allRooms.get(1)));
+
+        corridorsMap.add(new Corridor(allRooms.get(4), CommandType.NORD, true, allRooms.get(5)));
+        corridorsMap.add(new Corridor(allRooms.get(5), CommandType.SUD, false, allRooms.get(4)));
+
+        corridorsMap.add(new Corridor(allRooms.get(5), CommandType.NORD, true, allRooms.get(6)));
+        corridorsMap.add(new Corridor(allRooms.get(6), CommandType.SUD, false, allRooms.get(5)));
+
+        corridorsMap.add(new Corridor(allRooms.get(6), CommandType.NORD, true, allRooms.get(10)));
+        corridorsMap.add(new Corridor(allRooms.get(10), CommandType.SUD, false, allRooms.get(6)));
+
+        corridorsMap.add(new Corridor(allRooms.get(10), CommandType.NORD, false, allRooms.get(9)));
+        corridorsMap.add(new Corridor(allRooms.get(9), CommandType.SUD, false, allRooms.get(10)));
+
+        corridorsMap.add(new Corridor(allRooms.get(10), CommandType.OVEST, false, allRooms.get(8)));
+        corridorsMap.add(new Corridor(allRooms.get(8), CommandType.EST, false, allRooms.get(10)));
+
+        corridorsMap.add(new Corridor(allRooms.get(10), CommandType.EST, false, allRooms.get(7)));
+        corridorsMap.add(new Corridor(allRooms.get(7), CommandType.OVEST, false, allRooms.get(10)));
+
+        game.setCorridorsMap(corridorsMap);
+
+        Map<String, String> roomStates = new HashMap<>();
+
+        roomStates.put(allRooms.get(0).getName(), "Start");
+        roomStates.put(allRooms.get(1).getName(), "Buio");
+        roomStates.put(allRooms.get(2).getName(), "SerpentiOn");
+        roomStates.put(allRooms.get(3).getName(), "AcquaOn");
+        roomStates.put(allRooms.get(4).getName(), "Start");
+        roomStates.put(allRooms.get(5).getName(), "OcchioIntero");
+        roomStates.put(allRooms.get(6).getName(), "");
+        roomStates.put(allRooms.get(7).getName(), "Start");
+        roomStates.put(allRooms.get(8).getName(), "TortaOff");
+        roomStates.put(allRooms.get(9).getName(), "Spento");
+        roomStates.put(allRooms.get(10).getName(), "SarcofagoChiuso");
+
+        game.setStatesMap(roomStates);
+        */
         String json = gson.toJson(game);
         Files.write(Paths.get("src/main/resources/LoadedGame.json"), json.getBytes());
     }
+
     public void ConvertRoomstoJson(List<Room> rooms) throws IOException {
         Gson gson = new Gson();
         String json = gson.toJson(rooms);
