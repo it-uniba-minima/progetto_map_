f8af98aa4c117b9fd65e1754754b2e955980703b
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-29 19:15:08 +0200

    Migliorata GUI dei Riconoscimenti

--- src/main/java/org/it/uniba/minima/GUI/RiconoscimentiGUI.java
+++ src/main/java/org/it/uniba/minima/GUI/RiconoscimentiGUI.java
@@ -1,4 +1,6 @@
 package org.it.uniba.minima.GUI;
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalButtonUI;
 import java.awt.*;
 
 public class RiconoscimentiGUI extends javax.swing.JPanel {
@@ -12,6 +14,16 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon img = new ImageIcon("docs/img/placeholder_immagine sfondo.jpeg");
+                Image image = img.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         goBack = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
@@ -25,9 +37,19 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
         jPanel1.setMinimumSize(new java.awt.Dimension(800, 600));
         jPanel1.setPreferredSize(new java.awt.Dimension(800, 600));
 
-        goBack.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
-        goBack.setText("<--");
-        goBack.setMargin(new java.awt.Insets(0, 0, 8, 0));
+        goBack.setUI(new MetalButtonUI() {
+            protected Color getSelectColor () {
+                return new Color(133, 106, 5, 50);
+
+            }
+        });
+        goBack.setFocusPainted(false);
+        goBack.setBackground(new Color(204, 173, 27));
+        goBack.setForeground(new Color(255, 255, 255));
+        goBack.setBorderPainted(true);
+        goBack.setBorder(BorderFactory.createLineBorder(new Color(107, 90, 13), 5));
+        goBack.setFont(goBack.getFont().deriveFont(26f));
+        goBack.setText("⮜");
         goBack.setMaximumSize(new java.awt.Dimension(40, 40));
         goBack.setMinimumSize(new java.awt.Dimension(40, 40));
         goBack.setPreferredSize(new java.awt.Dimension(40, 40));
@@ -37,7 +59,16 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
             }
         });
 
-        jPanel2.setBackground(new java.awt.Color(242, 242, 142));
+        jPanel2 = new javax.swing.JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon img = new ImageIcon("docs/img/MarcoIcon.png");
+                Image image = img.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         jPanel2.setMinimumSize(new java.awt.Dimension(150, 150));
         jPanel2.setPreferredSize(new java.awt.Dimension(150, 150));
 
@@ -52,7 +83,16 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
                         .addGap(0, 150, Short.MAX_VALUE)
         );
 
-        jPanel3.setBackground(new java.awt.Color(242, 142, 242));
+        jPanel3 = new javax.swing.JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon img = new ImageIcon("docs/img/PascoIcon.png");
+                Image image = img.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         jPanel3.setMinimumSize(new java.awt.Dimension(150, 150));
         jPanel3.setPreferredSize(new java.awt.Dimension(150, 150));
 
@@ -67,7 +107,16 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
                         .addGap(0, 150, Short.MAX_VALUE)
         );
 
-        jPanel4.setBackground(new java.awt.Color(142, 242, 242));
+        jPanel4 = new javax.swing.JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon img = new ImageIcon("docs/img/MikIcon.png");
+                Image image = img.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         jPanel4.setMinimumSize(new java.awt.Dimension(150, 150));
         jPanel4.setPreferredSize(new java.awt.Dimension(150, 150));
 
@@ -82,22 +131,30 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
                         .addGap(0, 150, Short.MAX_VALUE)
         );
 
-        jLabel1.setFont(new java.awt.Font("Modern No. 20", 0, 36)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Papyrus", 0, 34));
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setOpaque(true);
+        jLabel1.setBackground(new Color(51, 44, 7, 169));
         jLabel1.setText("Riconoscimenti");
         jLabel1.setPreferredSize(new java.awt.Dimension(220, 39));
 
+        jTextArea1.setBackground(new Color(51, 44, 7, 169));
+        jTextArea1.setForeground(new Color(255, 255, 255));
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(50);
         jTextArea1.setRows(5);
         jTextArea1.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mattis elit in lacus faucibus\npretium. Sed a iaculis nulla. Nam sed ligula et orci imperdiet varius at sit amet libero.\nSed quis nisi quis lectus malesuada tincidunt sit amet pulvinar ligula. Proin tellus turpis,\nimperdiet consequat nibh nec, pharetra bibendum ante. Integer imperdiet quis metus sit\namet laoreet.  Duis a mauris mattis, auctor turpis nec, luctus risus. Fusce tempus dui id enim\nosuere, a congue arcu auctor. Praesent eu dui eu mauris tristique lobortis.");
         jTextArea1.setAutoscrolls(false);
-        jTextArea1.setBorder(null);
         jTextArea1.setCaretColor(new java.awt.Color(242, 242, 242));
         jTextArea1.setDisabledTextColor(new java.awt.Color(242, 242, 242));
         jTextArea1.setFocusable(false);
+        jTextArea1.setBorder(null);
         jTextArea1.setMinimumSize(new java.awt.Dimension(100, 80));
         jTextArea1.setSelectedTextColor(new java.awt.Color(242, 242, 242));
+        jTextArea1.setMargin(new Insets(0, 0, 0, 0));
         jScrollPane1.setViewportView(jTextArea1);
+        jScrollPane1.setBorder(null);
+
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -121,7 +178,7 @@ public class RiconoscimentiGUI extends javax.swing.JPanel {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(286, 286, 286))
+                                                .addGap(300, 300, 300))
                                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addGap(160, 160, 160))))
