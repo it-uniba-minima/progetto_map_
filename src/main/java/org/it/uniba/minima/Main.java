99fd9597ffeed0d6d5cba8889a0d9ed2f65cf076
Apan <marco.sm00327@gmail.com>
2024-06-01 15:37:10 +0200

    Database Funzionante con .sql di controllo

--- src/main/java/org/it/uniba/minima/Main.java
+++ src/main/java/org/it/uniba/minima/Main.java
@@ -1,5 +1,9 @@
 package org.it.uniba .minima;
+import org.it.uniba.minima.Boundary.outputDisplayManager;
 import org.it.uniba.minima.GUI.GUIManager;
+import org.it.uniba.minima.Database.DatabaseConnection;
+
+import java.sql.Connection;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,5 +12,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         new GUIManager();
+
+        String sql_query = DatabaseConnection.querySQL_forDESC("0", "Desert", "Start", "0", "0", "0");
+        Connection conn;
+        conn = DatabaseConnection.connect();
+        outputDisplayManager.displayText(DatabaseConnection.getDescriptionFromDatabase(conn, sql_query));
+        DatabaseConnection.close(conn);
+
     }
 }
