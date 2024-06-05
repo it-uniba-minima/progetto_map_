b36528a87411ddd6b988ab0d6b44c04d1f9e904d
Pascoooo <pacucciniko@gmail.com>
2024-06-05 14:03:27 +0200

    Sistemato HangmanGame più modulare e aggiunta la possibilità di commettere errori da parte dell'utente

--- src/main/java/org/it/uniba/minima/Boundary/HangmanGame.java
+++ src/main/java/org/it/uniba/minima/Boundary/HangmanGame.java
@@ -3,39 +3,69 @@ package org.it.uniba.minima.Boundary;
 import org.it.uniba.minima.GUI.ImpiccatoGUI;
 
 public class HangmanGame {
-    private static String guessingPhrase = "TI TROVI NELLA PIRAMIDE DI OSIRIDE";
-    private static ImpiccatoGUI impiccatoGUI = ImpiccatoGUI.getInstance();
+    private static final String GUESSING_PHRASE = "TI TROVI NELLA PIRAMIDE DI OSIRIDE";
+    private static final int PHRASE_LENGTH = 34;
+    private static final ImpiccatoGUI impiccatoGUI = ImpiccatoGUI.getInstance();
+    private static final int MAX_ATTEMPTS = 6;
+    private int currentAttempt = 0;
+    private char[] guessedLetters = new char[PHRASE_LENGTH];
 
-    public static void HangmanFlow(String text) {
+    public void HangmanFlow(String text) {
         String newText = text.toUpperCase();
         if (newText.length() == 1) {
-            checkLetter(newText);
-        } else if (newText.length() > 1 && newText.length() < 34) {
-            outputDisplayManager.displayText("Si può fare o una lettera alla volta o indovinare la frase intera");
-        } else {
-            checkGuess(newText);
-        }
-    }
-
-    public static void checkGuess(String text) {
-        if (text.equals(guessingPhrase)) {
-            outputDisplayManager.displayText("Hai indovinato la frase! Complimenti!");
-            impiccatoGUI.setPhrase(guessingPhrase);
-        } else {
-            outputDisplayManager.displayText("Hai sbagliato la frase! Riprova!");
-        }
-    }
-
-    public static void checkLetter(String text) {
-        if (guessingPhrase.contains(text)) {
-            outputDisplayManager.displayText("Hai indovinato una lettera!");
-            for (int i = 0; i < guessingPhrase.length(); i++) {
-                if (guessingPhrase.charAt(i) == text.charAt(0)) {
-                    impiccatoGUI.setLetter(i, text.charAt(0), guessingPhrase);
-                }
+            if (!checkLetter(newText)) {
+                currentAttempt++;
+            }
+        } else if (newText.length() == PHRASE_LENGTH) {
+            if (!checkGuess(newText)) {
+                currentAttempt++;
             }
         } else {
+            outputDisplayManager.displayText("Si può inserire solo una lettera o la frase intera!");
+        }
+
+        if (currentAttempt >= MAX_ATTEMPTS) {
+            outputDisplayManager.displayText("Hai perso! La frase era: " + GUESSING_PHRASE);
+            impiccatoGUI.setPhrase(GUESSING_PHRASE);
+        }
+    }
+
+    public boolean checkGuess(String text) {
+        if (text.equals(GUESSING_PHRASE)) {
+            outputDisplayManager.displayText("Hai indovinato la frase! Complimenti!");
+            impiccatoGUI.setPhrase(GUESSING_PHRASE);
+            return true;
+        } else {
+            outputDisplayManager.displayText("Hai sbagliato la frase! Riprova!");
+            return false;
+        }
+    }
+
+    public boolean checkLetter(String text) {
+        char letter = text.charAt(0);
+        for (int i = 0; i < PHRASE_LENGTH; i++) {
+            if (guessedLetters[i] == letter) {
+                outputDisplayManager.displayText("Hai già indovinato questa lettera!");
+                return true;
+            }
+        }
+
+        boolean found = false;
+        for (int i = 0; i < GUESSING_PHRASE.length(); i++) {
+            if (GUESSING_PHRASE.charAt(i) == letter) {
+                guessedLetters[i] = letter;
+                impiccatoGUI.setLetter(GUESSING_PHRASE, letter);
+                found = true;
+            }
+        }
+
+        if (found) {
+            outputDisplayManager.displayText("Hai indovinato una lettera!");
+            impiccatoGUI.setLetter(GUESSING_PHRASE, letter);
+            return true;
+        } else {
             outputDisplayManager.displayText("La lettera inserita non è presente nella frase");
+            return false;
         }
     }
 }
