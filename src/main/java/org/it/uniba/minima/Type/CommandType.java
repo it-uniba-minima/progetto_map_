1d87ba5a44f6a8f494286384780b1afad5d8d9c2
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-05 21:47:55 +0200

    Modifica dell'enum in italiano

--- src/main/java/org/it/uniba/minima/Type/CommandType.java
+++ src/main/java/org/it/uniba/minima/Type/CommandType.java
@@ -1,21 +1,23 @@
 package org.it.uniba.minima.Type;
 
 public enum CommandType {
-    HELP, // Comando che mostra la lista di comandi disponibili
+    AIUTO, // Comando che mostra la lista di comandi disponibili
 
     NORD, // Comando per direzione Nord
     SUD, // Comando per direzione Sud
     EST, // Comando per direzione Est
     OVEST, // Comando per direzione Ovest
 
-    INVENTORY, // Comando per accedere all'inventario
-
-    DAI, // Comando per dare oggetti
-    DROP, // Comando per lasciare oggetti
     OSSERVA, // Comando per osservare oggetti, stanze, personaggi
-    TAKE, //Comando per prendere oggetti
-    USA, // Comando per usare oggetti e usare oggetti su altri oggetti
-    PARLA, // Comando per parlare con personaggi
 
+    INVENTARIO, // Comando per accedere all'inventario
+
+    PRENDI, //Comando per prendere oggetti
+    LASCIA, // Comando per lasciare oggetti
+    USA, // Comando per usare oggetti e usare oggetti su altri oggetti
     UNISCI, // Comando per fondere oggetti
+
+    PARLA, // Comando per parlare con personaggi
+    DAI, // Comando per dare oggetti
+
 }
