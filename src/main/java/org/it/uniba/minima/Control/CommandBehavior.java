54350fa086a6a8a3f0fa8362b159320e6e5babad
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-06 20:57:01 +0200

    Crezione JavaDoc e piccole modifiche

--- src/main/java/org/it/uniba/minima/Control/CommandBehavior.java
+++ src/main/java/org/it/uniba/minima/Control/CommandBehavior.java
@@ -1,10 +1,14 @@
 package org.it.uniba.minima.Control;
-
-import org.it.uniba.minima.Entity.Agent;
-import org.it.uniba.minima.Entity.Game;
-import org.it.uniba.minima.Type.CommandType;
 import org.it.uniba.minima.Type.ParserOutput;
 
+/**
+ * The interface of the command behavior.
+ */
 public interface CommandBehavior {
+    /**
+     * The method that executes the command.
+     *
+     * @param parsedText the parsed text
+     */
     void execute(ParserOutput parsedText);
 }
