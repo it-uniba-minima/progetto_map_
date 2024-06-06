77f984a884712b0f08db58ae20ade35ecdc290df
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-06 20:58:41 +0200

    Crezione JavaDoc e piccole modifiche

--- src/main/java/org/it/uniba/minima/Mixer.java
+++ src/main/java/org/it/uniba/minima/Mixer.java
@@ -4,11 +4,28 @@ import javax.sound.sampled.*;
 import static org.it.uniba.minima.GUI.GameGUI.musicButtonSetTextGame;
 import static org.it.uniba.minima.GUI.MenuGUI.musicButtonSetTextMenu;
 
+/**
+ * The class that manages the audio.
+ */
 public class Mixer extends Thread {
+    /**
+     * The clip of audio.
+     */
     private static Clip clip;
+    /**
+     * The running status of the mixer.
+     */
     private static boolean running = false;
+    /**
+     * The instance of the mixer.
+     */
     private static Mixer instance;
 
+    /**
+     * Gets instance.
+     *
+     * @return the instance
+     */
     public static Mixer getInstance()  {
         if (instance == null) {
             instance = new Mixer();
@@ -16,9 +33,11 @@ public class Mixer extends Thread {
         return instance;
     }
 
+    /**
+     * Overrides the run method of the thread.
+     */
     @Override
     public void run() {
-        // TODO: Change the placeholder audio file with the actual audio file
         running = true;
         try {
             File file = new File("docs/audio/Desert_Menu-newSMB_DS.wav");
@@ -31,22 +50,37 @@ public class Mixer extends Thread {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * Starts the audio clip.
+     */
     public static void startClip() {
         running = true;
         reverseIcones();
         clip.start();
     }
 
+    /**
+     * Stops the audio clip.
+     */
     public static void stopClip() {
         running = false;
         reverseIcones();
         clip.stop();
     }
 
+    /**
+     * Returns the running status of the mixer.
+     *
+     * @return the running status
+     */
     public static boolean isRunning() {
         return running;
     }
 
+    /**
+     * Changes the icons of the music buttons.
+     */
     public static void reverseIcones() {
         if (!running) {
             musicButtonSetTextGame("🔇");
