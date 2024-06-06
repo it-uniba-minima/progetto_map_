d50bc7e4eeb554f4dccf855c616b466c8eb50419
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-06 20:56:56 +0200

    Crezione JavaDoc e piccole modifiche

--- src/main/java/org/it/uniba/minima/GUI/BoxLetter.java
+++ src/main/java/org/it/uniba/minima/GUI/BoxLetter.java
@@ -1,42 +1,57 @@
 package org.it.uniba.minima.GUI;
+import javax.swing.JTextField;
+import javax.swing.text.AbstractDocument;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 
-import javax.swing.*;
-import javax.swing.text.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-
+/**
+ * The type Box letter.
+ */
 public class BoxLetter {
-    private int row;
-    private int col;
+    /**
+     * The text field of the box.
+     */
     private JTextField textField;
 
-    public BoxLetter(int row, int col, Wordle parent) {
-        this.row = row;
-        this.col = col;
+    /**
+     * Instantiates a new box letter.
+     */
+    public BoxLetter() {
         textField = new JTextField(1); // Single character input
         textField.setFont(new Font("Papyrus", Font.BOLD, 30));
         textField.setHorizontalAlignment(JTextField.CENTER);
         ((AbstractDocument) textField.getDocument()).setDocumentFilter(new SingleCharDocumentFilter());
     }
-    public JTextField getBox() {
+
+    /**
+     * Returns the textfield of the box.
+     *
+     * @return the textfield
+     */
+    public JTextField getTextField() {
         return textField;
     }
 
-    protected void setBox( Color color) {
+    /**
+     * Sets the background color of the box.
+     *
+     * @param color the color
+     */
+    protected void setBoxColor(Color color) {
         textField.setBackground(color);
     }
+
+    /**
+     * Underclass to filter the input of the textfield to a single character.
+     */
     private static class SingleCharDocumentFilter extends DocumentFilter {
         @Override
         public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
             if ((fb.getDocument().getLength() + string.length()) <= 1 && string.matches("[A-Z]")) {
                 super.insertString(fb, offset, string, attr);
-            }
-        }
-        @Override
-        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-            if ((fb.getDocument().getLength() - length + text.length()) <= 1 && text.matches("[A-Z]")) {
-                super.replace(fb, offset, length, text, attrs);
             }
         }
     }
