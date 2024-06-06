af6f7e46f78da59298a2c22c226ee0059e9a05e4
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-06 20:58:12 +0200

    Crezione JavaDoc e piccole modifiche DA TERMINARE

--- src/main/java/org/it/uniba/minima/Boundary/TriviaGame.java
+++ src/main/java/org/it/uniba/minima/Boundary/TriviaGame.java
@@ -1,22 +1,35 @@
 package org.it.uniba.minima.Boundary;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.it.uniba.minima.Control.userInputFlow;
+import org.it.uniba.minima.Control.UserInputFlow;
 import org.it.uniba.minima.Database.DatabaseConnection;
 import org.it.uniba.minima.Entity.Game;
-
 import java.io.*;
 import java.net.*;
 
+/**
+ * The class that manages the trivia game.
+ */
 public class TriviaGame {
+    /**
+     * The Possible true answers.
+     */
     public static String[] possibleTrueAnswers = {"True", "T", "Vero", "V", "Si", "S"};
+    /**
+     * The Possible false answers.
+     */
     public static String[] possibleFalseAnswers = {"False", "F", "Falso", "No", "N"};
+    /**
+     * The constant correctAnswers.
+     */
     public static int correctAnswers = 0;
     private static String question;
     private static String correctAnswer;
 
+    /**
+     * Gets q and a.
+     */
     public static void getQAndA() {
         String urlToRead = "https://opentdb.com/api.php?amount=3&difficulty=easy&type=boolean";
         int maxAttempts = 3;
@@ -43,12 +56,17 @@ public class TriviaGame {
             } catch (IOException e) {
                 if (attempt == maxAttempts - 1) {
                     outputDisplayManager.displayText("> Scusa nipotino, l'età fa brutti scherzi e non riesco a ricordare la domanda. Ti dispiace ricominciare? (Il numero di domande a cui hai risposto correttamente me lo ricordo però!)");
-                    userInputFlow.Event = 0;
+                    UserInputFlow.Event = 0;
                 }
             }
         }
     }
 
+    /**
+     * Check guess.
+     *
+     * @param guess the guess
+     */
     public static void checkGuess(String guess) {
         int guessType = checkAnswerIsLegit(guess);
         if (guessType == 3) {
@@ -65,7 +83,7 @@ public class TriviaGame {
                 DatabaseConnection.printFromDB("0", "Stanza6", "Corretto", "Mummia", "0", "0");
                 game.setRoomState("Stanza6", "Corretto");
                 game.unlockCorridor("Stanza6", "Stanza10");
-                userInputFlow.Event = 0;
+                UserInputFlow.Event = 0;
             } else {
                 outputDisplayManager.displayText("> \"Bravo nipotino, è corretto! Ancora " + (3 - correctAnswers) + ".\"");
             }
@@ -74,10 +92,16 @@ public class TriviaGame {
             Game game = Game.getInstance();
             DatabaseConnection.printFromDB("0", "Stanza6", "Sbagliato", "Mummia", "0", "0");
             game.setRoomState("Stanza6", "Sbagliato");
-            userInputFlow.Event = 0;
+            UserInputFlow.Event = 0;
         }
     }
 
+    /**
+     * Check answer is legit int.
+     *
+     * @param text the text
+     * @return the int
+     */
     public static int checkAnswerIsLegit(String text) {
         for (String s : possibleTrueAnswers) {
             if (text.equalsIgnoreCase(s)) {
@@ -92,6 +116,11 @@ public class TriviaGame {
         return 3;
     }
 
+    /**
+     * Display question.
+     *
+     * @param question the question
+     */
     public static void displayQuestion(String question) {
         outputDisplayManager.displayText("> " + question);
         outputDisplayManager.displayText("> Rispondi con Vero o Falso");
