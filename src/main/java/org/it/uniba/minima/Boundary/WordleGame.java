7449e88ac14a54942e92660225b9d70ce030a7d9
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-06 10:57:06 +0200

    Sistemazione gioco Wordle definitiva

--- src/main/java/org/it/uniba/minima/Boundary/WordleGame.java
+++ src/main/java/org/it/uniba/minima/Boundary/WordleGame.java
@@ -1,33 +1,22 @@
 package org.it.uniba.minima.Boundary;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import org.it.uniba.minima.Control.GameManager;
 import org.it.uniba.minima.Control.userInputFlow;
 import org.it.uniba.minima.Database.DatabaseConnection;
 import org.it.uniba.minima.Entity.Game;
 import org.it.uniba.minima.GUI.GameGUI;
 import org.it.uniba.minima.GUI.Wordle;
-
-import javax.xml.crypto.Data;
-import java.awt.*;
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 public class WordleGame {
-    protected final int MaxAttempts = 6;
-    protected int currentTry = 0;
-    protected final int MaxLetters = 5;
-    protected String GuessingWord;
-    protected String[] charGuessingWord = GuessingWord.split("");
-    private WordleGame instance;
+    private static final int MAX_LETTERS = 5;
+    private static final int MAX_ATTEMPTS = 6;
+    private int currentTry = 0;
+    private final String GuessingWord;
+    private String[] charGuessingWord;
 
     public WordleGame() throws IOException {
         StringBuilder result = new StringBuilder();
@@ -40,21 +29,14 @@ public class WordleGame {
                 result.append(line);
             }
         }
-        GuessingWord = result.toString().replace("[", "").replace("]", "").replace("\"", "").replace("&quot;", "\"").replace("&amp;", "&").replace("&apos;", "'").replace("&lt;", "<").replace("&gt;", ">").replace("&#039;", "'").toUpperCase();
-    }
-
-    public void checkAttempts (int attempts) {
-        if (attempts > MaxAttempts) {
-           outputDisplayManager.displayText("Sei a corto di tentativi, la parola era " + GuessingWord);
-        }
-        //return 0;
+        GuessingWord = result.toString().replace("[", "").replace("]", "").replace("\"", "").toUpperCase();
     }
 
     public void printSplittedText(String text) {
         if(checkLenght(text)){
-            String[] newText = SplittedText(text);
+            String[] newText = text.split("");
             Wordle game = GameGUI.getWordle();
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < MAX_LETTERS; j++) {
                 game.setBoxText(currentTry, j, newText[j]);
             }
             checkGuess(text);
@@ -71,7 +53,7 @@ public class WordleGame {
             return;
         }
 
-        if (currentTry == MaxAttempts) {
+        if (currentTry == MAX_ATTEMPTS) {
             DatabaseConnection.printFromDB("0", "Desert", "Sbagliato", "Sfinge", "0", "0");
             outputDisplayManager.displayText("> \"La parola corretta era: " + GuessingWord + "!\"");
             userInputFlow.Event = 0;
@@ -82,15 +64,11 @@ public class WordleGame {
     }
 
     public boolean checkLenght(String text) {
-        if (text.length() != 5) {
-            outputDisplayManager.displayText("La parola inserita non è valida, inserire una parola di 5 lettere.");
+        if (text.length() != MAX_LETTERS) {
+            outputDisplayManager.displayText("La parola inserita non è valida, inserire una parola di MAX_LETTERS lettere.");
             return false;
         }
         return true;
-    }
-
-    public String[] SplittedText(String text) {
-        return text.split("");
     }
 
     public void changeBoxColor(int row, int col, Color color) {
@@ -100,18 +78,29 @@ public class WordleGame {
 
     public void checkGuess(String guess) {
         String[] charAlreadyGuessed = guess.split("");
-        for(int i = 0; i < 5; i++) {
+        charGuessingWord = GuessingWord.split("");
+
+        for(int i = 0; i < MAX_LETTERS; i++) {
             if (charGuessingWord[i].equals(charAlreadyGuessed[i])) {
-                changeBoxColor(currentTry, i, new Color(24, 159, 10));
+                changeBoxColor(currentTry, i, new Color(23, 147, 10));
                 charGuessingWord[i] = "";
+                charAlreadyGuessed[i] = "";
             }
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (charGuessingWord[i].equals(charAlreadyGuessed[j])) {
-                    changeBoxColor(currentTry, i, new Color(255, 0, 0));
+
+        for (int i = 0; i < MAX_LETTERS; i++) {
+            for (int j = 0; j < MAX_LETTERS; j++) {
+                if (charGuessingWord[i].equals(charAlreadyGuessed[j]) && !charGuessingWord[i].isEmpty()) {
+                    changeBoxColor(currentTry, j, new Color(178, 149, 22));
                     charGuessingWord[i] = "";
+                    charAlreadyGuessed[j] = "";
                 }
+            }
+        }
+
+        for (int i = 0; i < MAX_LETTERS; i++) {
+            if (!charAlreadyGuessed[i].isEmpty()) {
+                changeBoxColor(currentTry, i, new Color(47, 55, 61));
             }
         }
     }
