68d5b36e83c3c710bab7ec5257c2b3e43a428883
MikiMik88 <michelepontrelli8@gmail.com>
2024-06-07 18:09:23 +0200

    Sistemati Javadoc e piccoli problemi vari

--- src/main/java/org/it/uniba/minima/Boundary/TriviaGame.java
+++ src/main/java/org/it/uniba/minima/Boundary/TriviaGame.java
@@ -1,5 +1,4 @@
 package org.it.uniba.minima.Boundary;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.it.uniba.minima.Control.UserInputFlow;
@@ -13,26 +12,35 @@ import java.net.*;
  */
 public class TriviaGame {
     /**
-     * The Possible true answers.
+     * The aliases of the 'true' answer.
      */
     public static String[] possibleTrueAnswers = {"True", "T", "Vero", "V", "Si", "S"};
     /**
-     * The Possible false answers.
+     * The aliases of the 'false' answer.
      */
     public static String[] possibleFalseAnswers = {"False", "F", "Falso", "No", "N"};
     /**
-     * The constant correctAnswers.
+     * The number of correct answers so far.
      */
     public static int correctAnswers = 0;
+    /**
+     * The question.
+     */
     private static String question;
+    /**
+     * The correct answer.
+     */
     private static String correctAnswer;
 
     /**
-     * Gets q and a.
+     * Gets the question and the answer from the API.
      */
     public static void getQAndA() {
         String urlToRead = "https://opentdb.com/api.php?amount=3&difficulty=easy&type=boolean";
         int maxAttempts = 3;
+
+        // Try to get the question and answer from the API
+        // if it fails 3 times, the player will have to talk to the mummy again
         for (int attempt = 0; attempt < maxAttempts; attempt++) {
             try {
                 URL url = new URL(urlToRead);
@@ -47,15 +55,19 @@ public class TriviaGame {
                     }
                 }
 
-                Gson gson = new Gson();
                 JsonObject jsonObject = JsonParser.parseString(result.toString()).getAsJsonObject();
-                question = jsonObject.getAsJsonArray("results").get(0).getAsJsonObject().get("question").getAsString().replace("&quot;", "\"").replace("&amp;", "&").replace("&apos;", "'").replace("&lt;", "<").replace("&gt;", ">").replace("&#039;", "'").replace("&eacute;", "é").replace("&egrave;", "è");
+
+                question = jsonObject.getAsJsonArray("results").get(0).getAsJsonObject().get("question").getAsString().replace("&quot;", "\"")
+                                     .replace("&amp;", "&").replace("&apos;", "'").replace("&lt;", "<")
+                                     .replace("&gt;", ">").replace("&#039;", "'").replace("&eacute;", "é").replace("&egrave;", "è");
                 correctAnswer = jsonObject.getAsJsonArray("results").get(0).getAsJsonObject().get("correct_answer").getAsString();
+
                 displayQuestion(question);
+
                 break;
             } catch (IOException e) {
                 if (attempt == maxAttempts - 1) {
-                    outputDisplayManager.displayText("> Scusa nipotino, l'età fa brutti scherzi e non riesco a ricordare la domanda. Ti dispiace ricominciare? (Il numero di domande a cui hai risposto correttamente me lo ricordo però!)");
+                    OutputDisplayManager.displayText("> Scusa nipotino, l'età fa brutti scherzi e non riesco a ricordare la domanda. Ti dispiace ricominciare? (Il numero di domande a cui hai risposto correttamente me lo ricordo però!)");
                     UserInputFlow.Event = 0;
                 }
             }
@@ -63,44 +75,57 @@ public class TriviaGame {
     }
 
     /**
-     * Check guess.
+     * Checks the guess of the player.
      *
      * @param guess the guess
      */
     public static void checkGuess(String guess) {
         int guessType = checkAnswerIsLegit(guess);
+
+        // If the answer is not valid, the player has to insert it again
         if (guessType == 3) {
-            outputDisplayManager.displayText("> Risposta non valida, riprova");
+            OutputDisplayManager.displayText("> Risposta non valida, riprova");
             return;
         }
 
+        // If the answer is valid, assign the correct value to the guess
         guess = (guessType == 1) ? "True" : "False";
 
+        // Check if the answer is correct
+        // If it is, increment the number of correct answers and check if the trivia is won
+        // Otherwise, the trivia is lost
         if (guess.equalsIgnoreCase(correctAnswer)) {
             correctAnswers++;
+
+            // If the player has answered correctly 3 times, the game is won
+            // Otherwise, the player has to answer another question
             if (correctAnswers == 3) {
                 Game game = Game.getInstance();
+
                 DatabaseConnection.printFromDB("0", "Stanza6", "Corretto", "Mummia", "0", "0");
+
                 game.setRoomState("Stanza6", "Corretto");
                 game.unlockCorridor("Stanza6", "Stanza10");
                 UserInputFlow.Event = 0;
             } else {
-                outputDisplayManager.displayText("> \"Bravo nipotino, è corretto! Ancora " + (3 - correctAnswers) + ".\"");
+                OutputDisplayManager.displayText("> \"Bravo nipotino, è corretto! Ancora " + (3 - correctAnswers) + ".\"");
             }
         } else {
             correctAnswers = 0;
             Game game = Game.getInstance();
+
             DatabaseConnection.printFromDB("0", "Stanza6", "Sbagliato", "Mummia", "0", "0");
+
             game.setRoomState("Stanza6", "Sbagliato");
             UserInputFlow.Event = 0;
         }
     }
 
     /**
-     * Check answer is legit int.
+     * Checks if the answer is valid.
      *
-     * @param text the text
-     * @return the int
+     * @param text the answer
+     * @return the type of the answer (1 = true, 2 = false, 3 = invalid)
      */
     public static int checkAnswerIsLegit(String text) {
         for (String s : possibleTrueAnswers) {
@@ -117,12 +142,12 @@ public class TriviaGame {
     }
 
     /**
-     * Display question.
+     * Display question on the text panel.
      *
      * @param question the question
      */
     public static void displayQuestion(String question) {
-        outputDisplayManager.displayText("> " + question);
-        outputDisplayManager.displayText("> Rispondi con Vero o Falso");
+        OutputDisplayManager.displayText("> " + question);
+        OutputDisplayManager.displayText("> Rispondi con Vero o Falso");
     }
 }
